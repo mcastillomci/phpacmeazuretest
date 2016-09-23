6c7d3e135756c8c62e8d46bd7737689e9fb8a5ff
mcastillomci <mcastillo@olimex.cl>
2016-09-23 11:35:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  </head>
  <body>
  <?php echo '<p>Hola Mundo</p>'; ?>
+ <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNzRkY2QzNzAtZWYxNS00NjUxLTk2OTEtZGU5MmIxZTk4OGIxIiwidCI6IjZlNGQ1NDE3LTE2M2QtNGU2Ni04M2Q3LWM2OGVjNzUyOGRlMyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
  <?php phpinfo(); ?>
  </body>
 </html>
